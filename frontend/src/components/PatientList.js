87e42b495370821630016c333ce3f75c990ee93c
asa-leholland <asaleholland@gmail.com>
2023-01-14 14:46:02 -0800

    FEAT: add table styling

--- frontend/src/components/PatientList.js
+++ frontend/src/components/PatientList.js
@@ -4,30 +4,32 @@ import './PatientList.css';
 
 function PatientList({ patients, onDelete, onEdit }) {
     return (
-        <table id="Patients">
-            <caption className="caption-styling">You can add/edit patients here</caption>
-            <thead className="table-header-styling">
-                <tr>
-                    <th>Name</th>
-                    <th>Age</th>
-                    <th>Weight</th>
-                    <th>Height</th>
-                    <th>Date</th>
-                    <th>Diagnosis</th>
-                    <th>Delete</th>
-                    <th>Edit</th>
-                </tr>
-            </thead>
-            <tbody>
-                {patients.map((patient, i) => 
-                    <Patient
-                        patient={patient} 
-                        key={i}
-                        onDelete={onDelete}
-                        onEdit={onEdit} 
-                    />)}
-            </tbody>
-        </table>
+        <div className="table-container">
+            <table id="Patients">
+                <caption className="caption-styling">You can add/edit patients here</caption>
+                <thead className="table-header-styling">
+                    <tr>
+                        <th>Name</th>
+                        <th>Age</th>
+                        <th>Weight</th>
+                        <th>Height</th>
+                        <th>Date</th>
+                        <th>Diagnosis</th>
+                        <th>Delete</th>
+                        <th>Edit</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {patients.map((patient, i) => 
+                        <Patient
+                            patient={patient} 
+                            key={i}
+                            onDelete={onDelete}
+                            onEdit={onEdit} 
+                        />)}
+                </tbody>
+            </table>
+        </div>
     );
 }
 
