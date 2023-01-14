0362c944c5419d19042093c306b1fb1b0a5b0d38
Cocorico101 <83224644+Cocorico101@users.noreply.github.com>
2023-01-13 17:24:09 -0700

    Fix error for info entry and add textbox

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,50 +11,41 @@ function App() {
     // {username: "test", email:"test", password:"test"}
   }
   return (
-    /*<div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>*/
     <div style={styles.container}>
-      <h4>My Form</h4>
+      <h4>Patient Information</h4>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <input type="username" {...register('username',{
+        <input type="name" {...register('name',{
             required: true,
             minLength: 6,
             maxLength: 20, 
           })}
-          placeholder="Username"
+          placeholder="John Doe"
           style={styles.input}
         />
         <input
-          type="email"
-          name="email"
-          {...register({
+          type="email"{...register("email",{
             required: true,
             pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
-
           })}
-          placeholder="Email"
+          placeholder="johndoe@email.com"
           style={styles.input}
         />
         <input
-          name="password"
-          ref={register}
-          placeholder="Password"
+          type="address"{...register("address",{
+            required: true,
+          })}
+          placeholder="Address"
           style={styles.input}
         />
+        <p>Details of patients' visit</p>
+        <textarea rows="10" cols="60" type="text" minLength="4"
+          maxLength="1200"{...register("text",{
+          required: true,
+        })}
+          placeholder="Enter diagnostic notes"
+        />
+        <br/>
+        <br/>
         <button type="submit">Submit</button>
       </form>
     </div>
