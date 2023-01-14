09514159b602a1a6f33a242310ad0976398cc5a3
Cocorico101 <83224644+Cocorico101@users.noreply.github.com>
2023-01-14 12:49:59 -0700

    Nested route for front page to doctor home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,89 +2,44 @@
 import './App.css';
 import React from 'react';
 //import {useForm} from "react-hook-form";
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Link, Routes, Route } from 'react-router-dom';
 import { useState } from 'react';
-import Navigation from './components/navigation';
+//import Navigation from './components/navigation';
 //import DoctorHomePage from './pages/DoctorHomePage';
 import DoctorAddPatientPage from './pages/DoctorAddPatientPage';
 import DoctorEditPatientPage from './pages/DoctorEditPatientPage';
 import FrontPage from './pages/FrontPage';
+import DoctorHomePage from './pages/DoctorHomePage';
 //import validator from 'validator';
 
 function App() {
   const [patient] = useState([]);
   return (
-    <BrowserRouter>
+    <>
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">FrontPage</Link>
+        </li>
+        <li>
+          <Link to="/Doctor">Doctor</Link>
+          </li>
+
+      </ul>
+    </nav>
+    
       <Routes>
-        <Route path="/" element={<Navigation />} />
         <Route path="/" element={<FrontPage />} />
-        <Route path="/doctor/add" element={<DoctorAddPatientPage />} />
-        <Route path="/doctor/edit/:id" element={<DoctorEditPatientPage patient={patient} />} />
+        <Route path="/Doctor" element={<DoctorHomePage/>} >
+          <Route path="./doctorhomepage/add-patient" element={<DoctorAddPatientPage />} />
+          <Route path="./doctorhomepage/edit-patient/:id" element={<DoctorEditPatientPage patient={patient} />} />
+        </Route>
       </Routes>
-    </BrowserRouter>
+    </>
   );
 }
 
-/*
-const { register, handleSubmit } = useForm();
-function onSubmit(data) {
-  console.log(data);
-  // {username: "test", email:"test", password:"test"}
-}
-return (
-  <div style={styles.container}>
-    <h4>Patient Information</h4>
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <input type="name" {...register('name',{
-          required: true,
-          minLength: 6,
-          maxLength: 20,
-        })}
-        placeholder="John Doe"
-        style={styles.input}
-      />
-      <input
-        type="email"{...register("email",{
-          required: true,
-          pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
-        })}
-        placeholder="johndoe@email.com"
-        style={styles.input}
-      />
-      <input
-        type="address"{...register("address",{
-          required: true,
-        })}
-        placeholder="Address"
-        style={styles.input}
-      />
-      <p>Details of patients' visit</p>
-      <textarea rows="10" cols="60" type="text" minLength="4"
-        maxLength="1200"{...register("text",{
-        required: true,
-      })}
-        placeholder="Enter diagnostic notes"
-      />
-      <br/>
-      <br/>
-      <button type="submit">Submit</button>
-    </form>
-  </div>
 
-);
-}
-
-const styles = {
-container: {
-  width: "80%",
-  margin: "0 auto",
-},
-input: {
-  width: "100%",
-},
-};
-
-*/
 export default App;
 
 
