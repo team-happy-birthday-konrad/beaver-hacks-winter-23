ee8ef78e91b00cacca0d7f8466ca7af590eabbb7
asa-leholland <asaleholland@gmail.com>
2023-01-15 03:15:37 -0800

    FEAT: make auth signin look real

--- frontend/src/pages/FrontPage.js
+++ frontend/src/pages/FrontPage.js
@@ -13,23 +13,11 @@ function FrontPage() {
             <img src={logo} alt='B++MedicEHR'/>
             <h2 className="welcome-message">Welcome to our electronic medical record system</h2>
             <div className="form-container">
-                <h3 className="form-header">Doctor Sign In</h3>
-                <form className="doctor-form">
-                    <label>
-                        Email:
-                        <input type="email" placeholder="Enter email" required/>
-                    </label>
-                    <label>
-                        Password:
-                        <input type="password" placeholder="Enter password" required/>
-                    </label>
-                    <button type="submit">Sign In</button>
-                </form>
-                <h3 className="form-header">Patient Sign In</h3>
+            <h3 className="form-header">Patient Sign In</h3>
                 <form className="patient-form">
                     <label>
-                        Email:
-                        <input type="email" placeholder="Enter email" required/>
+                         Patient Username:
+                        <input type="email" placeholder="Enter username" required/>
                     </label>
                     <label>
                         Password:
@@ -37,6 +25,20 @@ function FrontPage() {
                     </label>
                     <button type="submit">Sign In</button>
 </form>
+
+                <h3 className="form-header">Staff Sign In</h3>
+                <form className="doctor-form">
+                    <label>
+                        BMEHR Email:
+                        <input type="email" placeholder="Enter email ending with @bmehr.org" required/>
+                    </label>
+                    <label>
+                        Password:
+                        <input type="password" placeholder="Enter password" required/>
+                    </label>
+                    <button type="submit">Sign In</button>
+                </form>
+                
 </div>
 </div>
 );
