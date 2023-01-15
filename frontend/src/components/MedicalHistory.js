e6048814424de7466f647c38c8b6abc7ff31f84a
asa-leholland <asaleholland@gmail.com>
2023-01-15 04:00:49 -0800

    FEAT: add medical history labeling

--- frontend/src/components/MedicalHistory.js
+++ frontend/src/components/MedicalHistory.js
@@ -6,20 +6,34 @@ const mockMedicalHistory = require('./mockedMedicalHistory.json');
 function MedicalHistory() {
     return (
         <div className="medical-history-container">
-            <h2>Medical History</h2>
-            <div className="medical-history-entries">
-                {mockMedicalHistory.records.map((record, i) => (
-                    <div key={i} className="medical-history-entry">
-                        <div className="date">{record.date}</div>
-                        <div className="doctor-name">{record.doctorName}</div>
-                        <div className="department">{record.department}</div>
-                        <div className="diagnosis">{record.diagnosis}</div>
-                        <div className="medications">{record.medications}</div>
-                    </div>
-                ))}
-            </div>
+        <div className="medical-history-entries">
+        {mockMedicalHistory.records.map((record, i) => (
+        <div key={i} className="medical-history-entry">
+        <div className="date">
+        <label className="entry-label">Date: </label>
+        <span className="content">{record.date}</span>
         </div>
-    );
-}
+        <div className="doctor-name">
+        <label className="entry-label">Doctor: </label>
+        <span className="content">{record.doctorName}</span>
+        </div>
+        <div className="department">
+        <label className="entry-label">Department: </label>
+        <span className="content">{record.department}</span>
+        </div>
+        <div className="diagnosis">
+        <label className="entry-label">Diagnosis: </label>
+        <span className="content">{record.diagnosis}</span>
+        </div>
+        <div className="medications">
+        <label className="entry-label">Medications: </label>
+        <span className="content">{record.medications}</span>
+        </div>
+        </div>
+        ))}
+        </div>
+        </div>
+        );
+        }
 
 export default MedicalHistory;
