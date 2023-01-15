cc835d26a1c93bf3d96403a33bd8cfb3583f8a71
asa-leholland <asaleholland@gmail.com>
2023-01-15 02:37:24 -0800

    FEAT: show sign in page

--- frontend/src/pages/FrontPage.js
+++ frontend/src/pages/FrontPage.js
@@ -3,15 +3,41 @@ import React from 'react';
 //import {BiDrink} from 'react-icons/bi';
 //import {SiFoodpanda} from 'react-icons/si';
 
+import './FrontPage.css';
 
 function FrontPage() {
     return (
-        <>
-            <h2> Welcome to our record system</h2>
-            
-        </>
-    );
+        <div className="landing-page">
+            <h1 className="app-name">MediCare</h1>
+            <h2 className="welcome-message">Welcome to our electronic medical record system</h2>
+            <div className="form-container">
+                <h3 className="form-header">Doctor Sign In</h3>
+                <form className="doctor-form">
+                    <label>
+                        Email:
+                        <input type="email" placeholder="Enter email" required/>
+                    </label>
+                    <label>
+                        Password:
+                        <input type="password" placeholder="Enter password" required/>
+                    </label>
+                    <button type="submit">Sign In</button>
+                </form>
+                <h3 className="form-header">Patient Sign In</h3>
+                <form className="patient-form">
+                    <label>
+                        Email:
+                        <input type="email" placeholder="Enter email" required/>
+                    </label>
+                    <label>
+                        Password:
+                        <input type="password" placeholder="Enter password" required/>
+                    </label>
+                    <button type="submit">Sign In</button>
+</form>
+</div>
+</div>
+);
 }
-
 
 export default FrontPage;
