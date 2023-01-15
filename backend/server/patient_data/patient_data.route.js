80699520d14e08afb1eff7a63f5e87339cab0102
asa-leholland <asaleholland@gmail.com>
2023-01-15 08:57:49 -0800

    FEAT: add route for file upload

--- backend/server/patient_data/patient_data.route.js
+++ backend/server/patient_data/patient_data.route.js
@@ -2,9 +2,13 @@ const express = require('express');
 const validate = require('express-validation');
 const paramValidation = require('../../config/param-validation');
 const patient_datumCtrl = require('./patient_data.controller');
+const multer = require('multer');
+const csv = require('csv-parser');
+const fs = require('fs');
 
 const router = express.Router(); // eslint-disable-line new-cap
 
+const upload = multer();
 router.route('/')
   /** GET /api/patient_data - Get list of patient_data */
   .get(patient_datumCtrl.list)
@@ -24,5 +28,37 @@ router.route('/:patient_datumId')
 
 /** Load patient_datum when API with patient_datumId route parameter is hit */
 router.param('patient_datumId', patient_datumCtrl.load);
+
+
+router.post('/upload', upload.single('file'), (req, res) => {
+  if (!req.file) {
+      return res.status(400).send({
+          success: false,
+          message: 'Please select a file to upload'
+      });
+  }
+
+  if (req.file.mimetype !== 'text/csv') {
+      return res.status(400).send({
+          success: false,
+          message: 'Invalid file type. Please select a CSV file.'
+      });
+  }
+
+  fs.createReadStream(req.file.path)
+      .pipe(csv())
+      .on('data', (data) => {
+          // handle data here
+          console.log(data);
+      })
+      .on('end', () => {
+          // handle end of file here
+          console.log('CSV file successfully processed');
+      });
+  return res.status(200).send({
+      success: true,
+      message: 'File uploaded successfully'
+  });
+});
 
 module.exports = router;
