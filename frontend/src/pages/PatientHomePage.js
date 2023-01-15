fcf270df5e18d5b22a0d08a2b7bd950e56e31225
asa-leholland <asaleholland@gmail.com>
2023-01-14 17:59:17 -0800

    WIP: add jank patient view with uploader

--- frontend/src/pages/PatientHomePage.js
+++ frontend/src/pages/PatientHomePage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useState, useEffect, useLocation } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './DoctorHomePage.css';
+import './PatientHomePage.css';
 
 import FileUploader from '../components/FileUploader';
 console.log('hery ')
@@ -28,13 +28,36 @@ function PatientHomePage({ patientId }) {
 //   setPatient(mockPatient);
  const patient = mockPatient;
 
+ const birthdate = new Date(patient.birthdate);
+
+ const age = new Date().getFullYear() - birthdate.getFullYear();
+
+ const email = patient.email;
+
+ const phone = patient.phone;
+
+ const name = patient.name;
+
+ const orNA = (value) => {
+    if (value) {
+        return value;
+    } else {
+        return 'N/A';
+    }
+    }
+
   return (
-    <div>
+    <div className='patient-home-page'>
       <h1>Patient Home Page</h1>
       <h2>Contact Details</h2>
-      <p>Name: {patient.name}</p>
-      <p>Email: {patient.email}</p>
-      <p>Phone: {patient.phone}</p>
+      <p>Name: {orNA(name)}</p>
+      <p>Email: {orNA(email)}</p>
+      <p>Phone: {orNA(phone)}</p>
+        <p>Age: {orNA(age)}</p>
+        <h2>Medical History</h2>
+        <p>Diagnosis: {orNA(patient.diagnosis)}</p>
+        <p>Medications: {orNA(patient.medications)}</p>
+        
       <FileUploader />
       
     </div>
