ade1c00059eef570b09e7193f4450c7301b3e432
ArtofBugs <TheArtofBugs@gmail.com>
2023-01-15 00:05:28 -0800

    New file

---
+++ backend/server/patient_data/patient_data.model.js
@@ -0,0 +1,496 @@
+patient_dataconst Promise = require('bluebird');
+const mongoose = require('mongoose');
+const httpStatus = require('http-status');
+const APIError = require('../helpers/APIError');
+
+/**
+ * Patient_Datum Schema
+ */
+const Patient_DatumSchema = new mongoose.Schema({
+  date_array {
+     type: Date[]
+     required: true
+  }
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  patient_datum: {
+     type: String[],
+     required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+/**
+ * Add your
+ * - pre-save hooks
+ * - validations
+ * - virtuals
+ */
+
+/**
+ * Methods
+ */
+Patient_DatumSchema.method({
+});
+
+/**
+ * Statics
+ */
+Patient_DatumSchema.statics = {
+  /**
+   * Get patient_datum
+   * @param {ObjectId} id - The objectId of patient_datum.
+   * @returns {Promise<Patient_Datum, APIError>}
+   */
+  get(id) {
+    return this.findById(id)
+      .exec()
+      .then((patient_datum) => {
+        if (patient_datum;) {
+          return patient_datum;;
+        }
+        const err = new APIError('No such patient_datum; exists!', httpStatus.NOT_FOUND);
+        return Promise.reject(err);
+      });
+  },
+
+  /**
+   * List patient_data in descending order of 'createdAt' timestamp.
+   * @param {number} skip - Number of patient_data to be skipped.
+   * @param {number} limit - Limit number of patient_data to be returned.
+   * @returns {Promise<Patient_Datum[]>}
+   */
+  list({ skip = 0, limit = 50 } = {}) {
+    return this.find()
+      .sort({ createdAt: -1 })
+      .skip(+skip)
+      .limit(+limit)
+      .exec();
+  }
+};
+
+/**
+ * @typedef Patient_Datum
+ */
+module.exports = mongoose.model('Patient_Datum', Patient_DatumSchema);
